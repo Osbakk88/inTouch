ff5f7bf16acc583b1aab3e4fdf3eb143a46d184f
Osbakk88 <osbakk88@github.com>
2026-04-07 19:15:01 +0200

    Login page with error handling.

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -4,12 +4,22 @@ import { setToken, setUser } from "../utils/storage.js";
 
 export function initLoginPage() {
   const form = document.querySelector("#login-form");
+  const errorMessage = document.querySelector("#login-error");
   if (!form) return;
 
   form.addEventListener("submit", async (event) => {
     event.preventDefault();
     const data = getFormData(form);
-    console.log("Login submit", data);
+    errorMessage.textContent = "";
+
+    try {
+      const userData = await login(data);
+      setToken(userData.data.accessToken);
+      setUser(userData.data);
+      window.location.href = "./feed.html";
+    } catch (error) {
+      errorMessage.textContent = error.message;
+    }
   });
 }
 
