18b95206871a5fa0002f3e1f30b4840169ccca5a
Osbakk88 <osbakk88@github.com>
2026-04-07 19:52:59 +0200

    Post page set up.

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,2 +1,21 @@
-import { API_ENDPOINTS } from "../utils/config.js";
+import { API_ENDPOINTS, API_KEY } from "../utils/config.js";
 import { getToken } from "../utils/storage.js";
+
+export async function getPosts() {
+  const accessToken = getToken();
+
+  const response = await fetch(API_ENDPOINTS.posts.base, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "X-Noroff-API-Key": API_KEY,
+    },
+  });
+
+  const result = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Error, no post loding");
+  }
+
+  return result.data;
+}
