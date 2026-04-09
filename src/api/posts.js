2c50050b82880e1b5093f7744ea7da550f9fb5f1
Osbakk88 <osbakk88@github.com>
2026-04-09 18:58:47 +0200

    Added creat post function.

--- src/api/posts.js
+++ src/api/posts.js
@@ -14,7 +14,29 @@ export async function getPosts() {
   const result = await response.json();
 
   if (!response.ok) {
-    throw new Error("Error, no post loding");
+    throw new Error("Error, loading failed. Please try again.");
+  }
+
+  return result.data;
+}
+
+export async function createPost(payload) {
+  const accessToken = getToken();
+
+  const response = await fetch(API_ENDPOINTS.posts.base, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "X-Noroff-API-Key": API_KEY,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  });
+
+  const result = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Error, could not create post. Please try again.");
   }
 
   return result.data;
